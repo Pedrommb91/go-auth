7fbbb454dc15e5acffe0e545e0733798d44bcc38
pbarrei <pbarrei@mercedes-benz.com>
2023-06-22 12:13:47 +0100

    tests: encypt pkg tests

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/Pedrommb91/go-auth
 go 1.20
 
 require (
-	github.com/deepmap/oapi-codegen v1.13.0
 	github.com/getkin/kin-openapi v0.118.0
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
@@ -51,7 +50,6 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/invopop/yaml v0.1.0 // indirect
@@ -95,6 +93,7 @@ require (
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
---
+++ pkg/encrypt/encryptor_test.go
@@ -0,0 +1,54 @@
+package encrypt
+
+import (
+	"testing"
+
+	"github.com/Pedrommb91/go-auth/pkg/errors"
+)
+
+func TestPasswordEncryptor_Encrypt(t *testing.T) {
+	type args struct {
+		plaintext string
+		salt      string
+		encPass   string
+	}
+	tests := []struct {
+		name           string
+		pes            *PasswordEncryptor
+		args           args
+		wantEncryptErr error
+		wantDecryptErr error
+	}{
+		{
+			name: "Success on encryting/decrypting password",
+			pes:  &PasswordEncryptor{},
+			args: args{
+				plaintext: "very-strong-password",
+				salt:      "very-strong-salt",
+				encPass:   "very-strong-encoding-password",
+			},
+			wantEncryptErr: nil,
+			wantDecryptErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pes := &PasswordEncryptor{}
+			got, err := pes.Encrypt(tt.args.plaintext, tt.args.salt, tt.args.encPass)
+			if !errors.Equal(err, tt.wantEncryptErr) {
+				t.Errorf("PasswordEncryptor.Encrypt() error = %v, wantErr %v", err, tt.wantEncryptErr)
+				return
+			}
+			pw, err := pes.Decrypt(got, tt.args.salt, tt.args.encPass)
+			if !errors.Equal(err, tt.wantDecryptErr) {
+				t.Errorf("PasswordEncryptor.Decrypt() error = %v, wantErr %v", err, tt.wantDecryptErr)
+				return
+			}
+
+			if pw != tt.args.plaintext {
+				t.Errorf("PasswordEncryptor.Encrypt()/Decrypt() got = %v, want %v", pw, tt.args.plaintext)
+				return
+			}
+		})
+	}
+}
